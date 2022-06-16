b1aaa3a87805fe63368290ab726acb304da36883
yonatankumsa <yonatankumsa99@gmail.com>
2022-06-16 14:26:36 -0400

    yoni

--- controllers/artists.js
+++ controllers/artists.js
@@ -9,9 +9,9 @@ router.use((req, res, next) => {
     res.redirect("/users/login");
   }
 });
-    
- 
-fetch('https://api.napster.com/v2.2/playlists/top?apikey=NTZmZjkwMTEtODAxZC00MmYwLThlYWMtY2MxM2I5MzBiY2Qy&limit=40')
+  
+
+const api = fetch('https://api.napster.com/v2.2/playlists/top?apikey=NTZmZjkwMTEtODAxZC00MmYwLThlYWMtY2MxM2I5MzBiY2Qy&limit=40')
 .then(response => response.json())
 .then(response => {
 
